551aef3adc9e9da341d15301581d4442a3adaa24
Christoph Buelter <c.buelter@arcor.de>
2015-09-06 20:25:47 +0200

    Use lowercase names and break code where needed.

--- Sublime3dsMax.py
+++ Sublime3dsMax.py
@@ -1,5 +1,5 @@
-""" Send maxscript files or codelines to 3ds Max.
-"""
+"""Send maxscript files or codelines to 3ds Max."""
+
 from __future__ import unicode_literals
 
 import os
@@ -18,97 +18,101 @@ else:
 # Create the tempfile in "Packages" (ST2) / "Installed Packages" (ST3)
 TEMP = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    "Send_to_3ds_Max_Temp.ms"
-)
-NO_MXS_FILE = r"Sublime3dsMax: File is not a MAXScript file (*.ms, *.mcr)"
-NO_TEMP = r"Sublime3dsMax: Could not write to temp file"
-NOT_SAVED = r"Sublime3dsMax: File must be saved before sending to 3ds Max"
-MAX_NOT_FOUND = r"Sublime3dsMax: Could not find a 3ds max instance."
-RECORDER_NOT_FOUND = r"Sublime3dsMax: Could not find MAXScript Macro Recorder"
-NO_FILE = r"Sublime3dsMax: No file currently open"
+    "Send_to_3ds_Max_Temp.ms")
 
-MAX_TITLE_IDENTIFIER = r"Autodesk 3ds Max"
+TITLE_IDENTIFIER   = "Autodesk 3ds Max"
+PREFIX             = "Sublime3dsMax:"
+NO_MXS_FILE        = PREFIX + " File is not a MAXScript file (*.ms, *.mcr)"
+NO_TEMP            = PREFIX + " Could not write to temp file"
+NOT_SAVED          = PREFIX + " File must be saved before sending to 3ds Max"
+MAX_NOT_FOUND      = PREFIX + " Could not find a 3ds max instance."
+RECORDER_NOT_FOUND = PREFIX + " Could not find MAXScript Macro Recorder"
+NO_FILE            = PREFIX + " No file currently open"
 
 
-def isMaxscriptFile(file):
-    name, ext = os.path.splitext(file)
+def is_maxscriptfile(filepath):
+    name, ext = os.path.splitext(filepath)
     return ext in (".ms", ".mcr")
 
 
-def isPythonFile(file):
-    name, ext = os.path.splitext(file)
+def is_pythonfile(filepath):
+    name, ext = os.path.splitext(filepath)
     return ext in (".py")
 
 
-def sendCmdToMax(cmd):
-    """ Tries to find the 3ds Max window by title and
-    the mini macrorecorder by class. Sends a string command
-    and a return-key buttonstroke to it to evaluate the command.
+def send_cmd_to_max(cmd):
+    """Tries to find the 3ds Max window by title and the mini
+    macrorecorder by class.
+
+    Sends a string command and a return-key buttonstroke to it to
+    evaluate the command.
+
     """
-    gMainWindow = winapi.Window.find_window(MAX_TITLE_IDENTIFIER)
-    if gMainWindow is None:
+    mainwindow = winapi.Window.find_window(TITLE_IDENTIFIER)
+    if mainwindow is None:
         sublime.error_message(MAX_NOT_FOUND)
         return
 
-    gMiniMacroRecorder = gMainWindow.find_child(text=None, cls="MXS_Scintilla")
+    minimacrorecorder = mainwindow.find_child(text=None, cls="MXS_Scintilla")
     # If the mini macrorecorder was not found, there is a chance we are
     # in an ancient Max version (e.g. 9) where the recorder etc. was not
     # scintilla based, but instead a rich edit box.
-    if gMiniMacroRecorder is None:
-        gStatusPanel = gMainWindow.find_child(text=None, cls="StatusPanel")
-        if gStatusPanel is None:
+    if minimacrorecorder is None:
+        statuspanel = mainwindow.find_child(text=None, cls="StatusPanel")
+        if statuspanel is None:
             sublime.error_message(RECORDER_NOT_FOUND)
             return
-        gMiniMacroRecorder = gStatusPanel.find_child(text=None, cls="RICHEDIT")
+        minimacrorecorder = statuspanel.find_child(text=None, cls="RICHEDIT")
         # Verbatim strings (the @ at sign) are also not yet supported.
         cmd = cmd.replace("@", "")
         cmd = cmd.replace("\\", "\\\\")
 
-    if gMiniMacroRecorder is None:
+    if minimacrorecorder is None:
         sublime.error_message(RECORDER_NOT_FOUND)
         return
 
     sublime.status_message('Send to 3ds Max: {cmd}'.format(
         **locals())[:-1])  # Cut ';'
     cmd = cmd.encode("utf-8")  # Needed for ST3!
-    gMiniMacroRecorder.send(winapi.WM_SETTEXT, 0, cmd)
-    gMiniMacroRecorder.send(winapi.WM_CHAR, winapi.VK_RETURN, 0)
-    gMiniMacroRecorder = None
+    minimacrorecorder.send(winapi.WM_SETTEXT, 0, cmd)
+    minimacrorecorder.send(winapi.WM_CHAR, winapi.VK_RETURN, 0)
+    minimacrorecorder = None
 
 
-def saveToTempFile(text):
-    """ Stores code in a temporary maxscript file.
-    """
-    with open(TEMP, "w") as tempFile:
+def save_to_tempfile(text):
+    """Stores code in a temporary maxscript file."""
+    with open(TEMP, "w") as tempfile:
         if ST3:
-            tempFile.write(text)
+            tempfile.write(text)
         else:
             text = text.encode("utf-8")
-            tempFile.write(text)
+            tempfile.write(text)
 
 
 class SendFileToMaxCommand(sublime_plugin.TextCommand):
-    """ Sends the current file by using 'fileIn <file>'.
-    """
+    """Sends the current file by using 'fileIn <file>'."""
+
     def run(self, edit):
         currentfile = self.view.file_name()
         if currentfile is None:
             sublime.error_message(NOT_SAVED)
             return
 
-        if isMaxscriptFile(currentfile):
+        if is_maxscriptfile(currentfile):
             cmd = 'fileIn (@"{currentfile}")\r\n'.format(**locals())
-            sendCmdToMax(cmd)
-        elif isPythonFile(currentfile):
+            send_cmd_to_max(cmd)
+        elif is_pythonfile(currentfile):
             cmd = 'python.executefile (@"%s")\r\n' % currentfile
-            sendCmdToMax(cmd)
+            send_cmd_to_max(cmd)
         else:
             sublime.error_message(NO_MXS_FILE)
 
 
 class SendSelectionToMaxCommand(sublime_plugin.TextCommand):
-    """ Sends selected part of the file.
-        Selection is extended to full line(s).
+    """Sends selected part of the file.
+
+    Selection is extended to full line(s).
+
     """
     def run(self, edit):
         currentfile = self.view.file_name()
@@ -120,23 +124,24 @@ class SendSelectionToMaxCommand(sublime_plugin.TextCommand):
                 line = self.view.line(region)
                 text = self.view.substr(line)
                 cmd = '{text};'.format(**locals())
-                sendCmdToMax(cmd)
+                send_cmd_to_max(cmd)
 
             # Else send all lines where something is selected
             # This only works by saving to a tempfile first,
             # as the mini macro recorder does not accept multiline input
             else:
                 line = self.view.line(region)
-                self.view.run_command("expand_selection", {"to": line.begin()})
+                self.view.run_command("expand_selection",
+                                     {"to": line.begin()})
                 regiontext = self.view.substr(self.view.line(region))
-                saveToTempFile(regiontext)
+                save_to_tempfile(regiontext)
                 if os.path.exists(TEMP):
                     if currentfile:
-                        if isMaxscriptFile(currentfile):
+                        if is_maxscriptfile(currentfile):
                             cmd = 'fileIn (@"%s")\r\n' % TEMP
                         else:
                             cmd = 'python.executefile (@"%s")\r\n' % TEMP
-                        sendCmdToMax(cmd)
+                        send_cmd_to_max(cmd)
                     else:
                         sublime.error_message(NO_FILE)
                 else:
